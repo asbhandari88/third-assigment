2222ba7bf7648ad772f58eb5bb503e25e525ae3d
asbhandari88 <asb634@nyu.edu>
2018-05-23 14:18:28 -0400

    1.Assignment 3rd First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public content:string = "Secret Password - tuna";
+  public display:boolean = true;
+  public clicks:any = [];
+  public now:Date = new Date();
+
+  onClick(){
+    this.display = !this.display;
+    this.clicks.push("Content : " +this.content+" Display status: "+this.display+" Current timestamp : " +this.now );
+  }
+  getBackColor(index:number){
+    return (index>=4) ? 'blue':'none';
+  }
 }
